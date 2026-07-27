38d86c1eab25a15cc9df7ebbc6a02978de8bf11b
K4yan0 <237609883+K4yan0@users.noreply.github.com>
2026-07-27 17:38:13 +0200

    feat(conf): updating the config page with a connexion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  // Replace this with your actual GitHub Pages URL or custom domain
+  site: 'https://K4yan0.github.io',
+
+  // If your repo is named 'portfolio', uncomment the line below:
+  base: '/portfolio',
+
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [react()]
 });

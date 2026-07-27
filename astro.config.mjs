32ac4be3e5013818adaefc9fcff8cdf034ab691b
K4yan0 <237609883+K4yan0@users.noreply.github.com>
2026-07-27 18:18:39 +0200

    feat(lang): adding english and french text

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,18 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   // Replace this with your actual GitHub Pages URL or custom domain
-  site: 'https://K4yan0.github.io',
-
+  site: 'https://username.github.io',
+  
   // If your repo is named 'portfolio', uncomment the line below:
-  base: '/portfolio',
+  // base: '/portfolio',
+  
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fr'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
 
   vite: {
     plugins: [tailwindcss()]
